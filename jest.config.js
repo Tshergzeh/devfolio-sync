82d54d813703442dfb61286552742d4efb7c649a
Oluwasegun Ige <mailshegze@gmail.com>
2025-11-07 15:28:23 +0100

    fix .js issue on fetchWithLogging

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ export default {
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.ts", "**/tests/**/*.test.ts"],
   moduleNameMapper: {
+    "^@/(.*)\\.js$": "<rootDir>/src/$1",
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   moduleDirectories: ["node_modules", "src"],
--- src/utils/fetchWithLogging.ts
+++ src/utils/fetchWithLogging.ts
@@ -1,4 +1,4 @@
-import { logger } from "@/utils/logger";
+import { logger } from "@/utils/logger.js";
 
 export async function fetchWithLogging(url: string, options?: RequestInit) {
   const start = Date.now();
